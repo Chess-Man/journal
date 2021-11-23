c4c56449db21eabcefd5bfe92450856bee8fced8
Jericho <jerichobantiquete@gmail.com>
2021-11-23 23:35:23 +0800

    task file link

--- resources/views/links/subject.blade.php
+++ resources/views/links/subject.blade.php
@@ -5,6 +5,11 @@
         </h2>
         
 
+<nav class="my-6 flex items-center gap-8">
+  <a class="font-bold text-indigo-600 pb-2 text-lg border-b-2 border-indigo-700" href="">Task</a>
+  <a class="font-bold text-indigo-600 pb-2 text-lg border-indigo-700" href="">File</a>
+</nav>
+
 <header class="mt-8 mb-4 flex gap-2 item-center">   
   <input type="search"  class="focus:ring-indigo-500 py-2 focus:border-indigo-500 block w-72 pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="Search...">
   <a href="#" class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-800">
